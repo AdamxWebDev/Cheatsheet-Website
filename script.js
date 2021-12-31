c4e9c1b81c8d511f828a9f7ebc080c64455f590f
Adam WebDev <Adam_WebDev@outlook.com>
2021-12-30 19:18:10 -0700

    Cleaner Code

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-let table = document.getElementById('navigation');
+let navBox = document.getElementById('navigation');
 let button = document.getElementById('hide');
 let main = document.querySelector('main');
 let header = document.querySelector('header');
 let divBox = document.getElementById('nav-box');
-let table2 = document.getElementById('fixed-nav-box');
 let button2 = document.getElementById('show');
 
 let hideTable = () => {
-table.hidden = true;
-table2.removeAttribute("hidden")
+button.hidden = true;
+button2.removeAttribute("hidden")
 main.style.padding = "25px 100px";
 header.style.padding = "25px 100px";
+navBox.style.width = "300px";
+navBox.style.height = ""
+navBox.style.position = "static"
+navBox.style.left = ""
+navBox.style.top = ""
+};
+
+let showTable = () => {
+button.removeAttribute("hidden");
+button2.hidden = true;
+main.style.padding = "25px 95px 25px 210px";
+header.style.padding = "25px 95px 25px 210px";
+navBox.style.width = "200px";
+navBox.style.height = "98%";
+navBox.style.position = "fixed";
+navBox.style.left = "10px";
+navBox.style.top = "10px";
 };
 
 button.onclick = hideTable;
-
-
-
-
-let showTable = () => {
-table.removeAttribute("hidden");
-table2.hidden = true;
-main.style.padding = "25px 95px 25px 210px";
-header.style.padding = "25px 95px 25px 210px";
-};
-
 button2.onclick = showTable;
